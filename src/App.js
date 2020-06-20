c0a157e1d9e7b1986255009fbb6d85399921e602
ernesthenry <henry38ernest@gmail.com>
2020-06-20 18:12:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Quote extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data: '',
+      quote:'',
+      author:''
+    }
+  }
+  
+  componentDidMount(){
+    this.generateQuote();
+  }
+  
+  generateQuote = () => {
+    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+ .then(response => response.json())
+  .then(data => {
+    this.setState({
+      data: data,
+      
+    })
+    console.log(data);
+  })
+    
+  }
+  
+  handleClick = (e) => {
+  const randomIndex = Math.floor(Math.random() * 102);   
+
+    e.preventDefault()
+    this.setState({
+      quote: this.state.data.quotes[randomIndex].quote,
+      author:this.state.data.quotes[randomIndex].author
+      
+    })
+  }
+  render(){
+  return(
+    <div id='quote-box' className='quote-box'>
+      <div>
+      <p id='text'>{this.state.quote}</p>
+      <p id='author'>{this.state.author}</p>
+      <p className='my-quote'>Wanna see a quote?</p>
+      <button id='new-quote' onClick={this.handleClick} className='btn'>Generate  
+      </button>
+      
+      <a id='tweet-quote' href='twitter.com/intent/tweet'><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+      </div>
+     
+      
     </div>
-  );
+    
+  )
+}
 }
 
-export default App;
+export default Quote;
