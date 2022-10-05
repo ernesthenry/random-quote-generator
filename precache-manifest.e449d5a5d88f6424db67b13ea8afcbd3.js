de12d5a4d4ba39883311513ef5e565fd7023f7a7
ernesthenry <henry38ernest@gmail.com>
2022-10-04 17:29:35 -0700

    Updates

---
+++ precache-manifest.e449d5a5d88f6424db67b13ea8afcbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb2fc701178539a677deeaeb25735b1",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "c58c3dd990575a8bbe1f",
+    "url": "/random-quote-generator/static/css/main.08551bf3.chunk.css"
+  },
+  {
+    "revision": "525e4e8c6589afee802d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58c3dd990575a8bbe1f",
+    "url": "/random-quote-generator/static/js/main.b8a07f91.chunk.js"
+  },
+  {
+    "revision": "efbd8013faa4b47d47b4",
+    "url": "/random-quote-generator/static/js/runtime-main.04b377e6.js"
+  }
+]);
